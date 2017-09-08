d292f3fc1b7156c260d5dda1067e2dd8881891ca
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-08 04:05:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,19 @@ class Dog
 
   @@all = []
 
+  attr_accessor :name
+  
   def initialize(name)
-
+    @name = name
   end
 
   def self.clear_all
     @@all = []
+  end
+
+  def self.all? 
+    @@all.each do |name|
+    end
   end
 
 
